48ef7cfa070e9291cf48f24123290f96b6ae592c
Brian Aditya Hermansyah <brian.adityaherman@gmail.com>
2021-09-14 11:13:09 +0700

    Prediction on app.py

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 input = transform_image('/home/brianadit24/Downloads/AI_Research/LungCovid/COVID-190.png')
 
 model = LungCovNet(output_size=2)
+model.load_state_dict(torch.load('model/weights_best.pth', map_location=device))
+
+preds = model.predict(input)
+print(preds)
